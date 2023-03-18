f33c65b322b723bd0c824a120146fae29386f52c
kmachura-mika <katarzyna.machura-mika@opi.org.pl>
2023-03-18 08:59:56 +0100

    primary structure of the help chef app

---
+++ backend/src/main/java/com/app/helpchef/HelpChefApplication.java
@@ -0,0 +1,13 @@
+package com.app.helpchef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpChefApplication.class, args);
+	}
+
+}
