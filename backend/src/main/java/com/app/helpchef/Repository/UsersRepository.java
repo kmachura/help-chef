04805c02758795b466b2848960432b01fa9f6b72
Adam <adamkowalsky11@gmail.com>
2023-03-31 18:05:19 +0200

    Create layer of application, API for User class with Test of the email value and add properties of H2 database

---
+++ backend/src/main/java/com/app/helpchef/Repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.app.helpchef.Repository;
+
+import com.app.helpchef.Model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
