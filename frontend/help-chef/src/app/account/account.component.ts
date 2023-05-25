7e15b023fcbb95a6b2c4c9331fd9a8c869e2a566
Alexshailiha <Alexshailiha@gmail.com>
2023-05-26 00:32:56 +0200

    Add routerLink for all the pages and basic CSS

--- frontend/help-chef/src/app/account/account.component.ts
+++ frontend/help-chef/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
+  constructor(private elementRef: ElementRef) {}
 
+  toggleNavbar(): void {
+    const menu = this.elementRef.nativeElement.querySelector('.menu');
+    const navbar = this.elementRef.nativeElement.querySelector('.navbar');
+  
+    menu.addEventListener('click', () => {
+      navbar.classList.toggle('change');
+    });
+  }
+  ngOnInit(): void {
+    this.toggleNavbar();
+  }
+    
 }
--- frontend/help-chef/src/app/add-recipe/add-recipe.component.ts
+++ frontend/help-chef/src/app/add-recipe/add-recipe.component.ts
@@ -1,8 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
   styleUrls: ['./add-recipe.component.css'],
 })
-export class AddRecipeComponent {}
+export class AddRecipeComponent {
+  constructor(private elementRef: ElementRef) {}
+
+  toggleNavbar(): void {
+    const menu = this.elementRef.nativeElement.querySelector('.menu');
+    const navbar = this.elementRef.nativeElement.querySelector('.navbar');
+  
+    menu.addEventListener('click', () => {
+      navbar.classList.toggle('change');
+    });
+  }
+  ngOnInit(): void {
+    this.toggleNavbar();
+  }
+    
+}
